224b224ea6d1647acce5d62b113fcb00e191db2d
hopo28 <hopo28@gmail.com>
2015-02-19 11:59:17 +0000

    Update ver1.ino

--- ver1.ino
+++ ver1.ino
@@ -1,44 +1,102 @@
-#include <Servo.h> 
 #include "FastLED.h"
+//example recipe:
+//R1010R2020R4030EC
+/* - PINS - 
+54 digital pins
 
-#define LED_PIN 2
-#define NUM_VALVES 10//nano has a limit of 12, mega has a limit of 48
-#define NUM_LEDS NUM_VALVES+1
-#define V_OPEN 20
-#define V_CLOSE 120
+01 - B
+02 - LED
+03 - B
+04 - B
+05 - B
+06 - B
+07 - B
+08 - B
+09 - B
+10 - SD CS
+11 - 
+12 - 
+13 - 
+14 - 
+MEGA ONLY:
+15 - 
+16 - 
+17 - 
+18 - 
+19 - 
+20 - LCD SDA
+21 - LCD SCL
+22 - 
+23 - 
+24 - 
+25 - 
+26 - 
+27 - 
+28 - 
+29 - 
+30 - V
+31 - B
+32 - V
+33 - B
+34 - V
+35 - B
+36 - V
+37 - B
+38 - V
+39 - 
+40 - V
+41 - 
+42 - V
+43 - 
+44 - V
+45 - 
+46 - V
+47 - 
+48 - V
+49 - 
+50 - SD MOSI
+51 - SD MISO
+52 - SD CLK
+53 - 
+54 - 
+
+16 analogue pins, 8 for NANO
+A8 - P_PIN_A
+nano - 14+8=22
+mega - 54+16=70
+teensy++ 2.0 - 46
+teensy 3.1 - 34
+
+
+*/
+
+#define MAX_NUM_COCKTAILS 50
+#define MAX_NUM_REAGENTS 20
+#define MAX_NUM_MIXERS 10
+#define NUM_FEEDS 6//valves-4
 #define OPEN true
 #define CLOSE false
-#define FWD 1
-#define STOP 0
-#define REV 2
-#define P_PIN_A A1
-#define P_PIN_B A2
-#define PNUM_AIR 3
-#define PNUM_WATER 2
-#define PNUM_DUMP 1
-#define PNUM_OUT 0
-//reagents are 4 onwards
-#define PIPE_VOLUME 20//in ml
-#define DUMP_VOLUME 20//in ml
-#define PUMP_COEFICIENT 120//pure guess - time it takes to pump 1ml of liquid
 #define DEBUG true
-
+//LEDS
+#define LED_PIN 2
+#define NUM_LEDS 11//remember one for the pump
 //colour definitions
 CRGB C_OPEN = CRGB(000,255,000);//Green
 CRGB C_CLOSE = CRGB(255,000,000);//Red
 CRGB C_FWD = CRGB(000,255,000);//Green
 CRGB C_STOP= CRGB(255,000,000);//Red
 CRGB C_REV = CRGB(000,000,255);//Blue
-
 CRGB leds[NUM_LEDS];
-
-Servo V0,V1,V2,V3,V4,V5,V6,V7,V8,V9; //create servo objects
-unsigned char V_PIN[NUM_VALVES] = {30,32,34,36,38,40,42,44,46,48};//servo pins
-
 int brightness = 16;//was 16
 
-//static recipies for now (reagent, ml)
-unsigned char squash[] = {1,42};
+String feeds[NUM_FEEDS];
+String cocktails[MAX_NUM_COCKTAILS];//holders for cocktail names
+String cocktailFN[MAX_NUM_COCKTAILS];//holders for cocktail file names
+int NUM_COCKTAILS = 0;//initial value
+String reagents[MAX_NUM_REAGENTS];
+int NUM_REAGENTS = 0;//initial value
+String mixers[MAX_NUM_MIXERS];
+int NUM_MIXERS = 0;//initial value
 
 void setup()
 {
@@ -46,66 +104,54 @@ void setup()
   LEDS.setBrightness(brightness);
   initValves();
   initPump();
+  initButtons();
   if(DEBUG){Serial.begin(9600);}
+  initLCD();
+  initSD();
+
+  for(int i = 0; i < NUM_FEEDS; i++)//temp reagent names
+  {
+    feeds[i] = "RName ";
+    feeds[i] += (i+1);
+  }
+  if(DEBUG){printMenu();}
+  /*
+  Serial.println(cocktailFN[0]);
+  Serial.println(cocktailFN[1]);
+  Serial.println(cocktailFN[2]);
+  Serial.println(cocktailFN[3]);
+  */
+   
+  LCD_MainMenu(0);//0 means show with default options
 }
 
 void loop()
 {
   //what happens if you try to close and already closed valve?
+  //add in possibility to change brightness levels from serial/SD card/EEPROM??
   
-  //reagents 1,2,3,4,5,6
+  //SORT BUTTONS - done
+  //sort lcd menus - partial, still working on
+  //sort actually doing cocktails
+  //REM multplex motor control channels
+  
+  checkMenuButtons();
+//  checkButtons();//other buttons
+  if(DEBUG){
+    if (Serial.available() > 0)
+    {
+      doMenu(Serial.read());
+    }
+  }
+    
+    
+  /*
   dispense(squash[0],squash[1]);
   end();
   clean();
+  while(true){}//stop here
+  */
   
-// - - - - - TESTING - - - - 
-
-  //just open and close valve 0 for all eternity
-  valve(0,CLOSE);
-    //delay(500);
-  valve(1,CLOSE);
-    //delay(100);
-  valve(2,CLOSE);
-    //delay(100);
-  valve(3,CLOSE);
-    //delay(100);
-  
-  //valve(4,CLOSE);
-  //valve(5,CLOSE);
-  //valve(6,CLOSE);
-  //valve(7,CLOSE);
-  //valve(8,CLOSE);
-  //valve(9,CLOSE);
-  //delay(3000);
-  valve(0,OPEN);
-   // delay(500);
-  valve(1,OPEN);
-    //delay(100);
-  valve(2,OPEN);
-    //delay(100);
-  valve(3,OPEN);
-   // delay(100);
-    
-  //valve(4,OPEN);
-  //valve(5,OPEN);
-  //valve(6,OPEN);
-  //valve(7,OPEN);
-  //valve(8,OPEN);
-  //valve(9,OPEN);
-  //delay(3000);
-  
-/*
-//
-V0.write(180);  
-V1.write(180);  
-V2.write(180);  
-V3.write(180);  
-delay(1000);  
-V0.write(0);  
-V1.write(0);  
-V2.write(0);  
-V3.write(0);  
-delay(1000);  
-*/
 }
+
 
